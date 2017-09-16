fe8b2a6865386c5d7f28e4782094cdbc3e727aad
yanshuli <907454286@qq.com>
2017-09-16 14:51:48 +0800

    ddd git commit -m ddd

--- project/index.ios.js
+++ project/index.ios.js
@@ -17,7 +17,7 @@ export default class project extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Nativedddd
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
